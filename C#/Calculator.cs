cec29d206f94e06e7d3ff43f7746111f87212175
Stratia <97822999+Stratia@users.noreply.github.com>
2022-02-18 22:43:54 -0800

    Add files via upload

---
+++ C#/Calculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+//Note: Save to not get error's
+
+namespace Program{
+
+    class Calculator    
+    { 
+        static void Main()
+        {
+            Console.WriteLine("\nCalculator");
+            Console.WriteLine("Add: +");
+            Console.WriteLine("Subtract: -\n");
+
+            Console.WriteLine("Divide: /");
+            Console.WriteLine("Multiply: *");
+
+    
+            bool kn = false;
+            while(kn == false){
+
+                try{
+
+                    Console.WriteLine("\nFirst Number: ");
+
+                    int n1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Operator ()");
+                    string oper = Console.ReadLine();
+
+                    Console.WriteLine("Second Number: ");
+                    int n2 = int.Parse(Console.ReadLine());
+
+                    switch(oper){
+
+                    case "+":
+                    Console.WriteLine("");
+                    Console.WriteLine(n1+n2);
+                    break;
+
+                    case "/":
+                    Console.WriteLine("");
+                    Console.WriteLine(n1/n2);
+                    break;
+
+                    case "-":
+                    Console.WriteLine("");
+                    Console.WriteLine(n1-n2);
+                    break;
+
+                    case "*":
+                    Console.WriteLine("");
+                    Console.WriteLine(n1*n2);
+                    break;
+
+                }
+                }
+
+                catch{
+                    Console.WriteLine("Invalid Number(s)");
+                }
+                
+        }
+    }
+    }
+
+}
+
